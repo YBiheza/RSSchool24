e874ffd9bf522ea80e46bac321373ae2c0892d21
Yanita <bihezayanita@gmail.com>
2024-09-19 17:01:21 +0300

    feat: add html

--- audio-player/index.js
+++ audio-player/index.js
@@ -2,9 +2,30 @@ let cover = document.querySelector('.album-cover');
 let author = document.querySelector('.author');
 let songName = document.querySelector('.song-name');
 let audio = document.querySelector('.audio');
+let play = document.querySelector('.play');
+let pause = document.querySelector('.pause');
+
 
 let songs = ['dontstartnow', 'beyonce'];
+let authors = ['Dua Lipa', 'Beyonce'];
+let songsNames = ["Don't start now", 'Lemonade'];
+let pics = ['dontstartnow', 'lemonade'];
+
+let num = 1;
 
 function LoadSong(song) {
-     songName.innerHTML = song;
+     songName.innerHTML = `${songsNames[num]}`
+     author.innerHTML = `${authors[num]}`
+     audio.src = `./assets/audio/${song}.mp3`
+     cover.src = `./assets/img/${pics[num]}.png`
 }
+
+LoadSong(songs[0]);
+
+play.addEventListener('click', () => {
+    audio.play();
+})
+
+/*pause.addEventListener('click', () => {
+    audio.pause();
+})*/
